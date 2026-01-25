edc3a64f45ae7cf964dc2d11f0c73d07c7374834
LawlessCoder <130258573+LawlessCoder@users.noreply.github.com>
2026-01-24 23:10:01 -0500

    1.6: add buttons for providing feedback

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,17 @@
 import { useState } from 'react'
 
+const Button = ({setState, currentState, buttonText}) => {
+  const handleClick = () => {
+    const newState = currentState + 1;
+    setState(newState);
+  }
+
+  return (
+    <button onClick={handleClick}>{buttonText}</button>
+  )
+}
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +20,10 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button setState={setGood} currentState={good} buttonText="good" />
+      <Button setState={setNeutral} currentState={neutral} buttonText="neutral" />
+      <Button setState={setBad} currentState={bad} buttonText="bad" />
     </div>
   )
 }
