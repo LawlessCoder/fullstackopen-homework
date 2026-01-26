f306b8c26e120bbd0187d294285edacb13290f09
LawlessCoder <lawless@DESKTOP-AEN149M.localdomain>
2026-01-26 14:50:56 -0500

    1.6: move buttons to parent component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,31 +1,43 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Button = ({setState, currentState, buttonText}) => {
+const VotingStation = ({ states }) => {
+  return states.map((item) => (
+    <Button
+      key={item.text}
+      setState={item.setter}
+      currentState={item.value}
+      buttonText={item.text}
+    />
+  ));
+};
+
+const Button = ({ setState, currentState, buttonText }) => {
   const handleClick = () => {
     const newState = currentState + 1;
     setState(newState);
-  }
+  };
 
-  return (
-    <button onClick={handleClick}>{buttonText}</button>
-  )
-}
-
+  return <button onClick={handleClick}>{buttonText}</button>;
+};
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const states = [
+    { state: good, setter: setGood, text: "good" },
+    { state: neutral, setter: setNeutral, text: "neutral" },
+    { state: bad, setter: setBad, text: "bad" },
+  ];
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button setState={setGood} currentState={good} buttonText="good" />
-      <Button setState={setNeutral} currentState={neutral} buttonText="neutral" />
-      <Button setState={setBad} currentState={bad} buttonText="bad" />
+      <VotingStation states={states} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
