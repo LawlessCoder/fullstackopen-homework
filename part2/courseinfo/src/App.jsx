6250fbf410b201e57c81dc881ebb80f8d563ef29
LawlessCoder <lawless@DESKTOP-AEN149M.localdomain>
2026-01-26 15:40:49 -0500

    2.4: create ability to display multiple courses on same page

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -9,7 +9,7 @@ const Course = ({ course }) => {
 };
 
 const Header = ({ course_name }) => {
-  return <h1>{course_name}</h1>;
+  return <h2>{course_name}</h2>;
 };
 
 const Part = ({ part_name, num_of_exercises }) => {
@@ -49,34 +49,59 @@ const Total = ({ course_parts }) => {
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  };
+  const courses = [
+    {
+      id: 1,
+      name: "Half Stack application development",
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return (
+    <>
+      <h1>Web Development Curriculum</h1>
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
+    </>
+  );
 };
 
 export default App;
