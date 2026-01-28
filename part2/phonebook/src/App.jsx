dd92ccb2c733928b2cb0a7bd61a0ab04834c20b7
LawlessCoder <lawless@DESKTOP-AEN149M.localdomain>
2026-01-27 21:37:13 -0500

    2.7: warn user when person exists in phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,12 +6,20 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault();
-    const newPerson = {
-      name: newName,
-    };
 
-    setPersons(persons.concat(newPerson));
-    setNewName("");
+    const personInPhonebook = persons.find((person) => person.name === newName)
+      ? true
+      : false;
+
+    if (personInPhonebook) {
+      alert(`${newName} is already in phonebook`);
+    } else {
+      const newPerson = {
+        name: newName,
+      };
+      setPersons(persons.concat(newPerson));
+      setNewName("");
+    }
   };
 
   const handleNameChange = (event) => {
