4fb87dbd24ec2a57f1673af4ceadef641915873f
LawlessCoder <130258573+LawlessCoder@users.noreply.github.com>
2026-01-29 23:26:13 -0500

    2.9: implement search bar for user to filter contacts

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchFilter, setNewSearch] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -33,9 +38,17 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleSearchChange = (event) => {
+    setNewSearch(event.target.value);
+  };
+  
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={searchFilter} onChange={handleSearchChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -48,7 +61,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {persons
+        .filter(person => person.name.toLowerCase().includes(searchFilter.toLowerCase()))
+        .map(person => (
         <p key={person.name}>
           {person.name} {person.number}
         </p>
